6e4d3bc562ee45f53cd7480e1fe1bbde33b80e0c
serhiops <serrheylitvinenko@gnail.com>
2022-09-11 01:54:56 +0300

    modify models.py

--- shop/models.py
+++ shop/models.py
@@ -6,51 +6,51 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 class CustomUser(AbstractUser):
-    number_of_phone = models.CharField(max_length=15, verbose_name='number of phone', blank=True, null=True)
-    email_verify = models.BooleanField(default=False, verbose_name='is verify email')
+    number_of_phone = models.CharField(_('number of phone'), max_length=15, blank=True, null=True)
+    email_verify = models.BooleanField(_('is verify email'), default=False)
 
 class Category(models.Model):
-    name = models.CharField(max_length=64, verbose_name="name")
-    description = models.CharField(max_length=128,verbose_name="description")
-    slug = models.SlugField(max_length=64, verbose_name="URL",unique=True)
-    is_active = models.BooleanField(default=True, verbose_name='active')
-    image = models.ImageField(upload_to="images/categories/%Y/%m/%d", verbose_name="image" )
+    name = models.CharField(_('name'), max_length=64)
+    description = models.CharField(_('description'), max_length=128)
+    slug = models.SlugField(_('URL'),max_length=64,unique=True)
+    is_active = models.BooleanField(_('active'), default=True)
+    image = models.ImageField(_('image'), upload_to="images/categories/%Y/%m/%d")
     
     class Meta:
-        ordering = ("name",)
+        ordering = ('name',)
 
     def __str__(self) -> str:
         return self.name
 
 class Review(models.Model):
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="get_author", verbose_name="author")
-    text = models.TextField(max_length=1024, verbose_name="text")
-    created = models.DateTimeField(auto_now_add=True, verbose_name="created")
-    updated = models.DateTimeField(auto_now=True, verbose_name="updated")
-    is_active = models.BooleanField(default=True, verbose_name="active")
-    product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="get_prd", verbose_name="product")
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='get_author', verbose_name=_('author') )
+    text = models.TextField(_('text'), max_length=1024)
+    created = models.DateTimeField(_('created'), auto_now_add=True)
+    updated = models.DateTimeField(_('updated'), auto_now=True)
+    is_active = models.BooleanField(_('active'), default=True)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='get_prd', verbose_name=_('product'))
 
     class Meta:
-        ordering = ("-created",)
+        ordering = ('-created',)
 
     def __str__(self) -> str:
         return self.author.username
 
 class Photo(models.Model):
-   image = models.ImageField(upload_to="images/products/%Y/%m/%d", verbose_name="image")
-   product = models.ForeignKey("Product",on_delete=models.CASCADE, related_name='photos')
+   image = models.ImageField(_('image'), upload_to="images/products/%Y/%m/%d")
+   product = models.ForeignKey('Product',on_delete=models.CASCADE, related_name='photos', verbose_name=_('product'))
 
 class Product(models.Model):
-    name = models.CharField(max_length=64, verbose_name="name")
-    description = models.TextField(verbose_name="description")
-    price = MoneyField(max_digits=14, decimal_places=2, default_currency='UAH', verbose_name="price")
-    is_active = models.BooleanField(default=True, verbose_name="active")
-    created = models.DateTimeField(auto_now_add=True, verbose_name="created")
-    updated = models.DateTimeField(auto_now=True, verbose_name="updated")
-    slug = models.SlugField(max_length=64, verbose_name="URL", unique=True)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT,related_name = "get_category", verbose_name="category")
-    views = models.ManyToManyField("Ip", related_name="post_views", blank=True)
-    discount = models.PositiveIntegerField(default=0, verbose_name='sale')
+    name = models.CharField(_('name'), max_length=64)
+    description = models.TextField(_('description'))
+    price = MoneyField(_('price'), max_digits=14, decimal_places=2, default_currency='UAH')
+    is_active = models.BooleanField(_('active'), default=True)
+    created = models.DateTimeField(_('created'), auto_now_add=True)
+    updated = models.DateTimeField(_('updated'), auto_now=True)
+    slug = models.SlugField(_('URL'),max_length=64,  unique=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT,related_name = 'get_category', verbose_name=_('category'))
+    views = models.ManyToManyField("Ip", related_name='post_views', blank=True, verbose_name=_('views'))
+    discount = models.PositiveIntegerField(_('sale'), default=0)
     
     class Meta:
         ordering = ("-created",)
@@ -63,9 +63,9 @@ class Product(models.Model):
         return self.name
 
 class Cart(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING, related_name="get_product", verbose_name="product")
-    added = models.DateTimeField(auto_now_add=True, verbose_name="added")
-    user = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING, related_name="get_fp")
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING, related_name="get_product", verbose_name=_('product'))
+    added = models.DateTimeField(_('added'), auto_now_add=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING, verbose_name=_('user'))
     class Meta:
         ordering = ("-added",)
 
@@ -76,10 +76,10 @@ class Ip(models.Model):
         return self.ip
 
 class Ordering(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="get_user", verbose_name="buyer")
-    product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="get_pr", verbose_name="product")
-    qty = models.PositiveIntegerField(default=1, verbose_name="qty")
-    created = models.DateTimeField(auto_now_add=True, verbose_name="created")
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="get_user",verbose_name=_('buyer'))
+    product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="get_pr", verbose_name=_('product'))
+    qty = models.PositiveIntegerField(_('qty'), default=1)
+    created = models.DateTimeField(_('created'), auto_now_add=True)
     class Meta:
         ordering = ("-created",)
 
