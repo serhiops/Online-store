a74cd64e75cf621b95c42b2c383f6b7b78607fec
serhiops <serrheylitvinenko@gnail.com>
2022-09-24 17:28:22 +0300

    fix js on create ordering

--- onlineStore/static/js/create_ordering.js
+++ onlineStore/static/js/create_ordering.js
@@ -4,6 +4,7 @@ let URL_POST = 'https://api.novaposhta.ua/v2.0/json/';
 $('#checkout_city').on('input', ()=>{
     let text = $('#checkout_city').val();
     if (text.length > 2){
+        $('#cityname>option').remove();
         let data = {
             "apiKey": KEY,
             "modelName": "Address",
@@ -15,46 +16,47 @@ $('#checkout_city').on('input', ()=>{
             },
             csrfmiddlewaretoken: getCookie('csrftoken')
         };
-    
         fetch(URL_POST, {
             method: 'POST',
-            headers: {
+            headers: { 
                 'Content-Type': 'application/json;charset=utf-8'
             },
             body: JSON.stringify(data)
         })
             .then(res => { return res.json() })
             .then(( { data } ) => {
-                let dataList = $('#cityname');
+                let dataList = $('#cityname'); 
+                cityName = data[0].Addresses[0].MainDescription;
                 data[0].Addresses.forEach(city => {
                     dataList.append(`<option value="${city.Present}">`);
                 });
-            });
-        
-            data = {
-                "apiKey": KEY,
-                "modelName": "Address",
-                "calledMethod": "getWarehouses",
-                "methodProperties": {
-                    "CityName": text,
-                    "Page": "1",
-                    "Language": "UA",
-                },
-                csrfmiddlewaretoken: getCookie('csrftoken')
-            };
 
-        fetch(URL_POST, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify(data)
-        })
-            .then(res => { return res.json() })
-            .then(( { data } ) => {
-                let postList = $('#postname');
-                data.forEach(postOficce => {
-                    postList.append(`<option value='${postOficce.Description}'>`)
+
+                data = {
+                    "apiKey": KEY,
+                    "modelName": "Address",
+                    "calledMethod": "getWarehouses",
+                    "methodProperties": {
+                        "CityName": cityName,
+                        "Page": "1",
+                        "Language": "UA",
+                    },
+                    csrfmiddlewaretoken: getCookie('csrftoken')
+                };
+            fetch(URL_POST, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(res => { return res.json() })
+                .then(( { data } ) => {
+                    let postList = $('#postname');
+                    $('#postname>option').remove();
+                    data.forEach(postOficce => {
+                        postList.append(`<option value='${postOficce.Description}'>`)
+                    });
                 });
             });
             $('#checkout_post').removeAttr('disabled');
